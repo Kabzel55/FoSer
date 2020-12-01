07fdacf250942783108f4a956fbd8a9086b645d1
Kabzel55 <patryk.koryga@pollub.edu.pl>
2020-12-01 15:38:18 +0100

    Timer

--- app/src/main/java/com/example/foser/SettingsActivity.java
+++ app/src/main/java/com/example/foser/SettingsActivity.java
@@ -10,12 +10,10 @@ public class SettingsActivity extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        if (savedInstanceState == null)
-        {
+        if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.settings, new SettingsFragment())
